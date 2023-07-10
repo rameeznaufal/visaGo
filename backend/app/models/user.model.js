e8d8e4135d775a566aafb22ad39dc0f2905257c4
rameeznaufal <rameeznaufal12@gmail.com>
2023-07-10 12:52:30 +0530

    renamed structure

---
+++ backend/app/models/user.model.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      store_name: {
+        type: Sequelize.STRING
+      },
+      lat: {
+        type: Sequelize.DOUBLE
+      },
+      lng: {
+        type: Sequelize.DOUBLE
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.DOUBLE(2,1)
+      },
+      descp: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return User;
+  };
---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+//require('dotenv').config()
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "merchant"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "buyer"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+// simple route
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
