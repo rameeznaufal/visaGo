0a493342484f96f2b2d077168ec9c4fdc6bb3604
rameeznaufal <rameeznaufal12@gmail.com>
2023-07-10 15:41:37 +0530

    removed user link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -79,13 +79,7 @@ class App extends Component {
               </li>
             )}
 
-            {currentUser && (
-              <li className="nav-item">
-                <Link to={"/user"} className="nav-link">
-                  User
-                </Link>
-              </li>
-            )}
+    
           </div>
 
           {currentUser ? (
